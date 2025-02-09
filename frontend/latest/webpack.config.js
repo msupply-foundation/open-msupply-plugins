146326057fa089ef36bf82a8bfb7e0192b0dfcad
Andrei <andrei.evguenov@gmail.com>
2025-02-09 17:17:15 +1300

    Relocate files to match newly defined structure

---
+++ frontend/latest/webpack.config.js
@@ -0,0 +1,13 @@
+const { merge } = require('webpack-merge');
+// Expecting for every plugin to be developed from 'common/packages/plugins/{plugin repo}/frontend/{version}'
+const generateBaseConfig = require('../../../plugin-webpack.config.js');
+const path = require('path');
+
+const pluginName = require('./package.json').name;
+const distDir = path.resolve(__dirname, 'dist');
+const extendConfig = {};
+
+module.exports = merge(
+  generateBaseConfig({ pluginName, distDir }),
+  extendConfig
+);
