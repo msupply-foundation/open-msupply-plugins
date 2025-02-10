0162c4a5f94cc3d8d3065f1d8fa998863b4ca025
Andrei <andrei.evguenov@gmail.com>
2025-02-10 16:34:43 +1300

    Add backend plugin

--- frontend/latest/webpack.config.js
+++ frontend/latest/webpack.config.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge');
 // Expecting for every plugin to be developed from 'common/packages/plugins/{plugin repo}/frontend/{version}'
-const generateBaseConfig = require('../../../plugin-webpack.config.js');
+const generateBaseConfig = require('../../../frontend-plugin-webpack.config.js');
 const path = require('path');
 
 const pluginName = require('./package.json').name;
