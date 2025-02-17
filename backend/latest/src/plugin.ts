8aae65b9b512bb3102a57593343775408f054535
Andrei <andrei.evguenov@gmail.com>
2025-02-18 12:27:36 +1300

    Update backend plugin comments

--- backend/latest/src/plugin.ts
+++ backend/latest/src/plugin.ts
@@ -1,21 +1,8 @@
 // To upload to server (from this dir):
 // cargo run --bin remote_server_cli -- generate-and-install-plugin-bundle -i './service/src/backend_plugin/examples/amc' -u 'http://localhost:8000' --username 'test' --password 'pass'
-// OR
-// cargo run --bin remote_server_cli -- generate-plugin-bundle -i './service/src/backend_plugin/examples/amc' -o 'check.json'
-// Can install via CLI
-// cargo run --bin remote_server_cli -- install-plugin-bundle --path 'check.json' -u 'http://localhost:8000' --username 'test' --password 'pass'
-// Or can install via curl
-// cargo run --bin remote_server_cli -- generate-plugin-bundle -i './service/src/backend_plugin/examples/amc' -o 'check.json'
-// curl -H 'Content-Type: application/json' --data '{"query":"query MyQuery {authToken(password: \"pass\", username: \"Admin\") {... on AuthToken {token}... on AuthTokenError {error {description}}}}","variables":{}}' 'http://localhost:8000/graphql'
-// TOKEN=token from above
-// curl --form files='@check.json' -v -H 'Cookie: auth={"token":"'$TOKEN'"}' 'http://localhost:8000/upload'
-// FILE_ID=file id from above
-// curl -H 'Authorization: Bearer '${TOKEN} -H 'Content-Type: application/json' --data '{"query":"query MyQuery { centralServer { plugin { uploadedPluginInfo(fileId: \"'$FILE_ID'\") {... on PluginInfoNode {backendPluginCodes}... on UploadedPluginError {error}}} }}","variables":{}}' 'http://localhost:8000/graphql'
-// Above would list codes in the bundle, next to add plugins
-// curl -H 'Authorization: Bearer '${TOKEN} -H 'Content-Type: application/json' --data '{"query":"mutation MyMutation { centralServer { plugins { installUploadedPlugin(fileId: \"'$FILE_ID'\") { backendPluginCodes } } } }" }' 'http://localhost:8000/graphql'
-// Above would add uploaded plugin to backend_plugin table and bind it
 
 import { BackendPlugins } from '@backendPlugins';
+// Tree shaking working
 import zipObject from 'lodash/zipObject';
 
 // TODO Should come from settings
@@ -52,8 +39,7 @@ const plugins: BackendPlugins = {
 
     sql_result.forEach(({ item_id, consumption }) => {
       response[item_id] = {
-        average_monthly_consumption:
-          consumption / (DAY_LOOKBACK / DAYS_IN_MONTH),
+        average_monthly_consumption: consumption / (DAY_LOOKBACK / DAYS_IN_MONTH),
       };
     });
 
